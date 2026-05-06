37113c39d6c74c1505295c34e4fe24aeda61cf6b
Kamil Rochala <rochalakamil2@gmail.com>
2026-05-06 15:57:26 +0200

    adding tests imports, and deal damage, take damage

--- server/classes/enemy.py
+++ server/classes/enemy.py
@@ -1,10 +1,32 @@
+import random
+
 from pydantic import (
     BaseModel
 )
 
 class Enemy(BaseModel):
-    max_hp: int
-    current_hp: int
+    max_hp: float
+    current_hp: float
     room_number: int
-    damage: int
-    armor: int
+    damage: float
+    armour: int
+
+    def take_damage(self, incoming_damage: int) -> int:
+        """
+        Apply damage to the enemy, accounting for armour.
+        Returns the actual damage taken.
+        """
+
+        actual_damage = incoming_damage - self.armour
+
+        self.current_hp = self.current_hp - actual_damage
+
+        return actual_damage
+    
+    def deal_damage(self) -> int:
+        if self.current_hp <= 0:
+            return 0
+        
+        variance = random.randint(-2, 2) # ±2 variance
+
+        return self.damage + variance
--- server/tests.py
+++ server/tests.py
@@ -1,2 +1,6 @@
 import unittest
-import classes.enemy
+
+from classes import (
+    enemy
+)
+
